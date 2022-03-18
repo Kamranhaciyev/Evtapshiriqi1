b06f32571eef7b1006cb8b60376c4d52cc883db3
Kamran Haciyev <68659359+Kamranhaciyev@users.noreply.github.com>
2022-03-18 11:22:23 +0400

    Ev tapshiriqi hazirdir

---
+++ EvTapshiriqi/EvTapshiriqi1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace EvTapshiriqi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 ci tapshiriq
+            //int a = 4;
+            //if (a % 3 == 0)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+            // 2 ci tapshiriq
+            //int a = 5;
+            //if (a % 3 == 0 && a > 9 && a < 100)
+            //{
+            //    Console.WriteLine("3-e bolunur cutdur");
+            //}
+            //else if (a % 3 == 0 && a < 9 && a > 0)
+            //{
+            //    Console.WriteLine("3-e bolunur tekdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("3-e bolunmur");
+            //}
+
+            //3 cu tapshiriq
+            //int a = 3;
+            //int b = 7;
+            //int c = a + b;
+            //if (c>9 && c<100)
+            //{
+            //    Console.WriteLine("cem iki reqemli ededdir");
+            //}
+            //else if (c > 99 && c < 1000)
+            //{
+            //    Console.WriteLine("cem uc reqemlidir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("nezere alinmayib");
+            //}
+
+            //4 cu tapshiriq
+            //int a = 4444;
+            //if (a > 0 && a < 10)
+            //{
+            //    Console.WriteLine("1 reqemli");
+            //}
+            //else if (a > 9 && a < 100)   
+            //{
+            //    Console.WriteLine("2 reqemli");
+            //}
+            //else if (a > 99 && a < 1000)   
+            //{
+            //    Console.WriteLine("3 reqemli");
+            //}
+            //else if (a > 999 && a < 10000)   
+            //{
+            //    Console.WriteLine("4 reqemli");
+            //}
+            //else if (a > 9999 && a < 100000)
+            //{
+            //    Console.WriteLine("5 reqemli");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Nezere alinmayib");
+            //}
+
+            //5 ci tapshiriq
+            //int a = 7;
+            //int b = 3;
+            //if (a%b==0)
+            //{
+            //    Console.WriteLine("Tam bolunur.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Tam bolunmur.");
+            //}
+        }
+    }
+}
